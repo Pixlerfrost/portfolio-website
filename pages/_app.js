d6a0a2ca2275e26691145e676232a13c29e25fc3
Mohamad Youness <pixlerfrost@gmail.com>
2022-12-22 16:03:48 -0500

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../Components/theme";
+import Font from "../Components/fonts";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <Font />
       <Component {...pageProps} />
     </ChakraProvider>
   );
